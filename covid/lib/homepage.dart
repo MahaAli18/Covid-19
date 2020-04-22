3494c6434d3872d6cc04484832657c8304bc1c3a
MahaAli18 <43891334+MahaAli18@users.noreply.github.com>
2020-04-22 18:42:44 +0500

    app changes

--- covid/lib/homepage.dart
+++ covid/lib/homepage.dart
@@ -1,7 +1,12 @@
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:tgd_covid_tracker/datasource.dart';
-import 'package:tgd_covid_tracker/pannels/worldwidepannel.dart';
+import 'package:tgd_covid_tracker/panels/worldwidepanel.dart';
+import 'package:tgd_covid_tracker/panels/mosteffectedcountries.dart';
+import 'package:tgd_covid_tracker/panels/infoPanel.dart';
+import 'package:tgd_covid_tracker/pages/countyPage.dart';
+import 'package:http/http.dart' as http;
+
 
 
 class HomePage extends StatefulWidget {
@@ -10,6 +15,36 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+    Map worldData;
+    fetchWorldData()async{
+      http.Response response = await http.get('https://corona.lmao.ninja/V2/all');
+       setState(() {
+        worldData = json.decode(response.body);
+     });
+    }
+
+     List countryData;
+      fetchCountryData() async {
+        http.Response response =
+        await http.get('https://corona.lmao.ninja/v2/countries?sort=cases');
+        setState(() {
+          countryData = json.decode(response.body);
+    });
+  }
+
+    @override
+    void initState() {
+    fetchWorldData();
+    fetchCountryData();
+    super.initState();
+  }
+
+
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,22 +58,61 @@ class _HomePageState extends State<HomePage> {
       ),
       ),
       body: SingleChildScrollView(child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
             height:100,
             alignment: Alignment.center,
-             padding: EdgeInsets.all(10),
+            padding: EdgeInsets.all(10),
             color: Colors.orange[100],
             child: Text(DataSource.quote,  style: TextStyle(
                   color: Colors.orange[800],
                   fontWeight: FontWeight.bold,
                   fontSize: 16),),
           ),
-          Text(
-                  'Worldwide',
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text('Worldwide', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,),),
+                GestureDetector(
+                      onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=>CountryPage()));
+                      },
+                      child: Container(
+                      decoration: BoxDecoration(
+                      color: primaryBlack,
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text('Regional', style: TextStyle(fontSize: 16, color: Colors.white , fontWeight: FontWeight.bold,),)),
                 ),
-          WorldwidePanel()
+              ],
+            ),
+
+          ),
+           worldData == null
+              ? CircularProgressIndicator()
+              : WorldwidePanel(
+                  worldData: worldData,
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Text('Most Affected Countries', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,),),
+                ),
+                SizedBox(height: 10,), 
+           countryData == null
+              ? Container()
+              : MostAffectedPanel(
+                  countryData: countryData,
+                ),
+
+             InfoPanel(),
+             SizedBox(height: 20,),
+             Center(child: Text("WE ARE TOGETHER IN THE FIGHT", style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold,),)), 
+             SizedBox(height: 50,)   
 
         ],
       )),
--- covid/lib/panels/worldwidepanel.dart
+++ covid/lib/panels/worldwidepanel.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 
-class WorldwidePanel extends StatelessWidget {
+class WorldwidePanel extends StatelessWidget{
   final Map worldData;
 
   const WorldwidePanel({Key key, this.worldData}) : super(key: key);
-
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Container(
       child: GridView(
         shrinkWrap: true,
         physics: NeverScrollableScrollPhysics(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, childAspectRatio: 2),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 2),
         children: <Widget>[
           StatusPanel(
             title: 'CONFIRMED',
@@ -36,15 +34,18 @@ class WorldwidePanel extends StatelessWidget {
             title: 'DEATHS',
             panelColor: Colors.grey[400],
             textColor: Colors.grey[900],
-            count: worldData['deaths'].toString(),
+             count: worldData['deaths'].toString(),
           ),
+
         ],
-      ),
+         ),
     );
+
   }
 }
 
-class StatusPanel extends StatelessWidget {
+class   StatusPanel extends StatelessWidget {
+
   final Color panelColor;
   final Color textColor;
   final String title;
@@ -54,30 +55,19 @@ class StatusPanel extends StatelessWidget {
       {Key key, this.panelColor, this.textColor, this.title, this.count})
       : super(key: key);
 
+  
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-
     return Container(
-      margin: EdgeInsets.all(10),
-      height: 80,
-      width: width / 2,
-      color: panelColor,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(
-                fontWeight: FontWeight.bold, fontSize: 16, color: textColor),
-          ),
-          Text(
-            count,
-            style: TextStyle(
-                fontSize: 16, fontWeight: FontWeight.bold, color: textColor),
-          )
-        ],
-      ),
+       margin: EdgeInsets.all(10),
+       height: 80, width: width/2 ,
+       color: panelColor,
+       child: Column(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children: <Widget>[Text(title, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: textColor)), Text(count, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: textColor))],
+         ),
+      
     );
   }
 }
